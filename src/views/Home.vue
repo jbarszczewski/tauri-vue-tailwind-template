cd141a054eb0dec1ba132bdc4d0d807ff312a1df
Jakub Barszczewski <jakub.barszczewski@outlook.com>
2021-05-01 18:08:40 +0100

    added bootstrap-vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,10 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+	<div class="home">
+		<img alt="Vue logo" src="../assets/logo.png" />
+		<HelloWorld
+			msg="Welcome to Your Vue2 + TypeScript + Tauri/Rust + Bootstrap App"
+		/>
+	</div>
 </template>
 
 <script lang="ts">
@@ -10,9 +12,9 @@ import Vue from "vue";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 export default Vue.extend({
-  name: "Home",
-  components: {
-    HelloWorld,
-  },
+	name: "Home",
+	components: {
+		HelloWorld,
+	},
 });
 </script>
