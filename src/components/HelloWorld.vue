bd056e64882af9f10490ef19291c4a38544b6e3b
Jakub Barszczewski <jakub.barszczewski@outlook.com>
2021-05-01 18:10:49 +0100

    added tauri

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,36 +1,35 @@
 <template>
-	<div>
-		<b-card
-			:title="msg"
-			img-src="https://picsum.photos/600/300/?image=25"
-			img-alt="Image"
-			img-top
-			tag="article"
-			style="max-width: 20rem"
-			class="mb-2"
-		>
-			<b-card-text>
-				Some quick example text to build on the card title and make up the bulk
-				of the card's content.
-			</b-card-text>
+  <div>
+    <b-card
+      :title="msg"
+      img-src="https://picsum.photos/600/300/?image=25"
+      img-alt="Image"
+      img-top
+      tag="article"
+      style="max-width: 20rem"
+      class="mb-2"
+    >
+      <b-card-text>
+        Some quick example text to build on the card title and make up the bulk
+        of the card's content.
+      </b-card-text>
 
-			<b-button @click="$router.push('about')" variant="primary"
-				>About</b-button
-			>
-		</b-card>
-	</div>
+      <b-button @click="$router.push('about')" variant="primary"
+        >About</b-button
+      >
+    </b-card>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 
 export default Vue.extend({
-	name: "HelloWorld",
-	props: {
-		msg: String,
-	},
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
 });
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
